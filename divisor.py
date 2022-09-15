98b1fe1e40615cc7f050488f09dc79af2bd55a09
ay-aki <55615907+ay-aki@users.noreply.github.com>
2022-09-16 00:36:45 +0900

    Create divisor.py

---
+++ divisor.py
@@ -0,0 +1,20 @@
+import math
+
+ret = []
+
+def proc(N, n):
+  ret.append(n)
+  if n**2 != N:
+    ret.append(N//n)
+
+# 約数列挙アルゴリズム
+def divisor(N):
+  sq_N = math.floor(math.sqrt(N))
+  for n in range(1, sq_N + 1):
+    if N % n == 0: # N/nとnは約数となる。
+      proc(N, n) 
+
+divisor(100)
+ret.sort()
+
+print(ret)
