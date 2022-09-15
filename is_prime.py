04965650c65669b6a4e463c266ca82580b96b53c
ay-aki <55615907+ay-aki@users.noreply.github.com>
2022-09-16 00:37:35 +0900

    Create is_prime.py

---
+++ is_prime.py
@@ -0,0 +1,11 @@
+import math
+
+# 素数判定 # O(√n)
+def is_prime(N):
+  sq_N = math.floor(math.sqrt(N))
+  for n in range(2, sq_N + 1):
+    if N % n == 0:
+      return False
+  return True
+
+print(is_prime(31)) # 31は素数か？
